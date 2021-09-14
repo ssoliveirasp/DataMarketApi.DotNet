0f8300e740d5bb643b404e66d43c5042b7ba5e70
Sandro Oliveira <ssoliveira@gmail.com>
2021-09-14 10:51:26 -0300

    Criação de diretorios

--- src/FlyApi.Api/Configurations/Application/apiConfigureApplication.cs
+++ src/FlyApi.Api/Configurations/Application/apiConfigureApplication.cs
@@ -1,19 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ApiExplorer;
-
 namespace FlyApi.Api.Configuration
 {
     public static partial class apiConfigureApplication
     {
         public  static void apiFlyConfigureApplication(this IApplicationBuilder application, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
-        {
+        {  
             application.addFlyConfiguration(env, provider);
             application.UseFlySwaggerUI(env, provider);
             application.addFlyException(env, provider);
